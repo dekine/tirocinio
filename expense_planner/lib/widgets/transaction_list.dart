b7f21b4b57ece4e1e37be13cd3acf2f7c54a6327
dekine <andrea.pugliatti@outlook.it>
2021-10-14 18:08:57 +0200

    Aggiungo immagine

--- expense_planner/lib/widgets/transaction_list.dart
+++ expense_planner/lib/widgets/transaction_list.dart
@@ -12,39 +12,56 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-              child: Row(children: <Widget>[
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                      color: Theme.of(context).primaryColor, width: 2)),
-              padding: const EdgeInsets.all(10),
-              child: Text(
-                '\$${transactions[index].amount.toStringAsFixed(2)}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Theme.of(context).primaryColor),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: transactions.isEmpty
+          ? Column(
               children: <Widget>[
                 Text(
-                  transactions[index].title,
+                  'No transactions added yet',
                   style: Theme.of(context).textTheme.headline6,
                 ),
-                Text(DateFormat.yMd().add_Hm().format(transactions[index].date),
-                    style: const TextStyle(color: Colors.grey)),
+                const SizedBox(
+                  height: 50,
+                ),
+                Image.asset('assets/images/waiting.png', fit: BoxFit.contain),
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                    child: Row(children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: Theme.of(context).primaryColor, width: 2)),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      '\$${transactions[index].amount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        transactions[index].title,
+                        style: Theme.of(context).textTheme.headline6,
+                      ),
+                      Text(
+                          DateFormat.yMd()
+                              .add_Hm()
+                              .format(transactions[index].date),
+                          style: const TextStyle(color: Colors.grey)),
+                    ],
+                  ),
+                ]));
+              },
+              itemCount: transactions.length,
             ),
-          ]));
-        },
-        itemCount: transactions.length,
-      ),
     );
   }
 }
