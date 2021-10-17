b5fb3dbe9216a1edd416ad02727fb7c9eca3f82e
dekine <andrea.pugliatti@outlook.it>
2021-10-17 17:38:48 +0200

    Aggiunte font

--- meals_app/lib/category_item.dart
+++ meals_app/lib/category_item.dart
@@ -7,18 +7,30 @@ class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
 
+  void selectCategory() {
+    // Navigator.of(context)
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7), color],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: selectCategory,
+      splashColor: Theme.of(context).colorScheme.primary,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
         ),
-        borderRadius: BorderRadius.circular(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.7), color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
       ),
     );
   }
