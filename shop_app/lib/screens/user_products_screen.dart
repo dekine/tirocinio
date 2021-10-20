d1c36ad24888a41ca66b1db91d34f36acd5e4ec3
dekine <andrea.pugliatti@outlook.it>
2021-10-20 18:26:26 +0200

    Aggiungo user_product_screen

---
+++ shop_app/lib/screens/user_products_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  const UserProductsScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                title: productsData.items[i].title,
+                imageUrl: productsData.items[i].imageUrl,
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
