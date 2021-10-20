73991d1e7d035775e9faed60b4d09bf70ba52fc5
dekine <andrea.pugliatti@outlook.it>
2021-10-20 12:53:11 +0200

    Aggiungo la order screen

--- shop_app/lib/main.dart
+++ shop_app/lib/main.dart
@@ -6,6 +6,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products_provider.dart';
 import './providers/cart_provider.dart';
+import './providers/orders.dart';
 
 void main() => runApp(const MyApp());
 
@@ -21,6 +22,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Orders(),
         ),
       ],
       child: MaterialApp(
