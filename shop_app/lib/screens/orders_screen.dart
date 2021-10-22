a167e75ff4edd1a668c0db1c92786fa30e380819
dekine <andrea.pugliatti@outlook.it>
2021-10-22 18:07:12 +0200

    Finito orders_screen

--- shop_app/lib/screens/orders_screen.dart
+++ shop_app/lib/screens/orders_screen.dart
@@ -11,7 +11,7 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
@@ -21,9 +21,27 @@ class OrdersScreen extends StatelessWidget {
           onPressed: () => Navigator.of(context).pushReplacementNamed('/'),
         ),
       ),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(order: orderData.orders[i]),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text('An error occurred!'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) =>
+                      OrderItem(order: orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
       ),
     );
   }
