24efd532fbb04203ec5c4e251ae9bf2bf15f1b21
dekine <andrea.pugliatti@outlook.it>
2021-10-25 11:33:54 +0200

    Primo commit per great_places_app

---
+++ great_places_app/lib/screens/places_list_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
